d27c6254ac6dd659a892a2207461ef2228306542
PetrJoe <akinjossy1298@gmail.com>
2024-10-25 02:48:56 +0100

    update with "python manage.py collectstatic --noinput "

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 python3 -m pip install -r requirements.txt
 python3 -m pip install --upgrade pip
-python3 manage.py collectstatic
+python manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
