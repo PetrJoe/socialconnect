5aa693981df057c447321ee01f50a0b4501dd554
Akanmu Akinkunmi <56254171+PetrJoe@users.noreply.github.com>
2024-10-25 11:37:21 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 python3 -m pip install -r requirements.txt
 python3 -m pip install --upgrade pip
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
