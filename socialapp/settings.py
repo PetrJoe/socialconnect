bfd7b5da56a92b28a6414f07081e45eb4a4662cc
PetrJoe <akinjossy1298@gmail.com>
2024-10-25 02:46:59 +0100

    change the db to dummy

--- socialapp/settings.py
+++ socialapp/settings.py
@@ -96,18 +96,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'socialapp.wsgi.application'
 
-
-
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.dummy',
+        'NAME': BASE_DIR / 'db.json',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
